e2c32709ab4bc766ef1546f58b705f93c9320835
Wyspywoods <zachary.peksa@gmail.com>
2024-04-01 20:02:54 -0400

    wyspifying 1.0.0

--- node_mappings.py
+++ node_mappings.py
@@ -7,16 +7,12 @@ NODE_CLASS_MAPPINGS = {
     ### LoRA Nodes    
     "CR Load LoRA": CR_LoraLoader,    
     "CR LoRA Stack": CR_LoRAStack,
-    "CR Random LoRA Stack": CR_RandomLoRAStack,
-    "CR Random Weight LoRA": CR_RandomWeightLoRA,
     "CR Apply LoRA Stack": CR_ApplyLoRAStack,  
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     ### LoRA Nodes    
-    "CR Load LoRA": "💊 CR Load LoRA",    
-    "CR LoRA Stack": "💊 CR LoRA Stack",
-    "CR Random LoRA Stack": "💊 CR Random LoRA Stack",
-    "CR Random Weight LoRA": "💊 CR Random Weight LoRA",
-    "CR Apply LoRA Stack": "💊 CR Apply LoRA Stack",  
+    "CR Load LoRA": "👻 CR Load LoRA",    
+    "CR LoRA Stack": "👻 CR LoRA Stack",
+    "CR Apply LoRA Stack": "👻 CR Apply LoRA Stack",  
 }
